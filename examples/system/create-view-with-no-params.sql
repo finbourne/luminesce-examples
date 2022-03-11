91c1c3ce0490c6cb18acf5f4f571d6c3a6c9d74c
Stephen LM <stephen.lemasney@finbourne.com>
2022-03-11 16:06:29 +0000

    updates

--- examples/system/create-view-with-no-params.sql
+++ examples/system/create-view-with-no-params.sql
@@ -1,28 +1,30 @@
--- #################### SUMMARY ####################
+-- ######################### SUMMARY #########################
 
 -- 1. This query shows you how to make a view
 -- 2. Luminesce views are effectively custom providers
 -- 3. You can think of them as being somewhat like stored procs
 
--- #################################################
-
+-- #########################################################
 
 -- First delete the view if it already exists
-
 @delete_model_portfolios_view =
 
 use Sys.Admin.SetupView
 --provider=Test.Example.TestHoldings
 --deleteProvider
 
-select 1 as deleting_view
+----
+
+select 1 as deleting
 
 enduse;
 
 -- Wait 5 seconds after delete before attempting to re-create
 -- This give the view time to reset on the Luminesce grid
+@delete_model_portfolios_view_response =
 
-@delete_model_portfolios_view_response = select * from delete_model_portfolios_view wait 5;
+select *
+from @delete_model_portfolios_view wait 5;
 
 -- The Sys.Admin.SetupView provider
 @model_portfolios_view =
@@ -38,23 +40,16 @@ enduse;
 
 select
     #distinct
-  #select
+  #select_agg
 {
-    {ModelPortfolioName^ : model_port_name}, -- we add a ^ to indicate this is a main field
+    {ModelPortfolioName^ : model_port_name},
     {Sector: sect},
     {Region: loc},
     {Weighting: weighting}
 }
 
 from @model_portfolios
-where #restrict
-
-{
-    {ModelPortfolioName+ : model_port_name},
-    {Sector: sect},
-    {Region: loc},
-    {Weighting: weighting}
-};
+where #restrict_agg
 
 enduse;
 
