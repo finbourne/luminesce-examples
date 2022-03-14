f95b5c6b0a9211b5ebb492ff3972f8d3d3111a37
Stephen LM <20342969+slemasne@users.noreply.github.com>
2022-03-14 12:22:27 +0000

    run a recon (#6)
    Co-authored-by: Stephen LM <stephen.lemasney@finbourne.com>

---
+++ examples/lusid/run-a-reconciliation/3-create-holdings.sql
@@ -0,0 +1,35 @@
+-- #################### SUMMARY ##############################
+
+-- 1. In this query, we  load holdings from an Excel
+--      file in LUSID
+
+-- ###########################################################
+
+@@file_date =
+
+select strftime('20220301');
+
+@@portfolioScope =
+
+select 'LuminesceReconExample';
+
+@holdings_from_excel =
+
+use Drive.Excel with @@file_date
+--file=/luminesce-recon-testing-folder/equity_holdings_{@@file_date}.xlsx
+--worksheet=lusid_holdings
+--addFileName
+enduse;
+
+-- Run holdings transformation
+@holdings_for_upload =
+
+select portfolio as [PortfolioCode], @@portfolioScope as [PortfolioScope], holding_date as
+   [EffectiveAt], 'EQ' || InstrumentId as [ClientInternal], units as [Units], Currency as
+   CostCurrency
+from @holdings_from_excel;
+
+-- Upload the transformed data into LUSID
+select *
+from Lusid.Portfolio.Holding.Writer
+where ToWrite = @holdings_for_upload;
