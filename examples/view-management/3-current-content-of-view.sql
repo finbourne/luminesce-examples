c71b8a1923a53f5539b13869563f9868b26b3dcd
Stephen LM <stephen.lemasney@finbourne.com>
2022-03-22 09:50:33 +0000

    view management

--- examples/view-management/3-current-content-of-view.sql
+++ examples/view-management/3-current-content-of-view.sql
@@ -2,7 +2,8 @@
 
 -- 1. This query shows you the contents of a view
 -- 2. The results alone cannot be used to recreate the view
--- 3. You will need to make some small modifications
+-- 3. You will need to query sys.logs.hcquery to get the SQL
+---   used to create  a view
 
 -- #################################################
 
