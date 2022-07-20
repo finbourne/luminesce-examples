18801c139f826573098aa753c256fde2e8ab4d7e
Stephen LeMasney <stephen.lemasney@finbourne.com>
2022-07-20 14:06:24 +0100

    SE-2315: add iif case

---
+++ examples/system/iif-and-case-when-statements.sql
@@ -0,0 +1,34 @@
+-- DESCRIPTION:
+-- This example shows you how to use iif and case when statements
+
+-- INSTRUCTIONS:
+-- Set a limit, which will decide if the file is a large or small file
+
+@@row_limit = select 10;
+
+-- Load data from LUSID Drive
+
+@data = use Drive.csv
+--file=/luminesce-examples/equity_transactions.csv
+enduse;
+
+-- Store scalar which is the count of rows in a file
+
+@row_count = select count(*) as [RowCount] from @data;
+
+-- Run an if statement to decide where to save files
+
+@@file_name = select iif(RowCount > @@row_limit, 'large-file', 'small-file') as [TargetFileLocation]
+from @row_count;
+
+-- This can also be achieved with a `case when` statement
+
+@@file_name_with_case = select case when RowCount > @@row_limit then 'large-file' else 'small-file' end as [TargetFileLocation]
+from @row_count;
+
+-- Save files to drive
+
+@save_to_drive = use Drive.SaveAs with @data, @@file_name
+--path=/luminesce-examples
+--fileNames={@@file_name}
+enduse;
