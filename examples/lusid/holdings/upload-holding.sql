22ae0e6ab5ba12a52894c3daa1e9b2e798b42c66
Matthew Fox <matthew.fox@finbourne.com>
2022-08-09 14:24:56 +0100

    SE-2340: Upload holdings from csv.

--- examples/lusid/holdings/upload-holding.sql
+++ examples/lusid/holdings/upload-holding.sql
@@ -1,12 +1,18 @@
 -- ============================================================
 
 -- Description:
--- 1. In this query, we upload a holding into LUSID.
--- 2. First, we specify the portfolio code and scope and date.
--- 3. Next, we create a table for the holding data.
+-- 1. In this query, we run an ETL process on holdings.
+-- 2. First, we load a CSV file of holdings from Drive.
+-- 3. Next, we transform the shape of the holdings data.
 -- 4. Finally we upload the holding data into LUSID.
 
 -- ============================================================
+
+-- Extract holding data from LUSID Drive
+@holding_data =
+use Drive.Csv
+--file=/luminesce-examples/holdings.csv
+enduse;
 
 -- Set variables for the portfolio's scope and code
 @@portfolio_scope =
@@ -19,13 +25,21 @@ select 'uk-equity';
 @@today =
 select date ('now');
 
--- Create table for an equity holding 
-@holding_data =
-select @@portfolio_scope as PortfolioScope, @@portfolio_code as PortfolioCode, @@today as 
-   EffectiveAt, 'EQ77FCE7E4310C4' as ClientInternal, 100 as Units, 23.5 as CostPrice, 'GBP' as 
-   CostCurrency, #2022-05-01# as PurchaseDate, #2022-05-04# as SettleDate, 'Set' as WriteAction;
+-- Transform data using SQL
+@holdings = select 
+@@portfolio_scope as PortfolioScope, 
+@@portfolio_code as PortfolioCode, 
+@@today as EffectiveAt, 
+ClientInternal as ClientInternal, 
+Units as Units, 
+Cost as CostPrice, 
+'GBP' as CostCurrency, 
+PurchaseDate as PurchaseDate, 
+SettleDate as SettleDate, 
+'Set' as WriteAction
+from @holding_data;
 
 -- Upload the holding data into LUSID
 select *
 from Lusid.Portfolio.Holding.Writer
-where toWrite = @holding_data;
+where toWrite = @holdings;
