8872b172ce6bcaae0147bdca8a380588e6152b27
fox8090 <56276220+fox8090@users.noreply.github.com>
2022-08-10 15:42:06 +0100

    SE-2341: Upload transactions from XML file.

--- examples/lusid/transactions/upload-transactions.sql
+++ examples/lusid/transactions/upload-transactions.sql
@@ -2,7 +2,7 @@
 
 -- Description:
 -- 1. In this query, we run an ETL process on some transactions.
--- 2. First, we load a CSV file of transactions from Drive.
+-- 2. First, we load a XML file of transactions from Drive.
 -- 3. Next, we transform the shape of the transaction data.
 -- 4. Finally we upload the transaction data into LUSID.
 
@@ -10,8 +10,19 @@
 
 -- Extract transaction data from LUSID Drive
 @txn_data = 
-use Drive.Csv
---file=/luminesce-examples/transactions.csv
+use Drive.Xml
+--file=/luminesce-examples/transactions.xml
+--nodePath=transactions/transaction
+--columns
+TransactionID=transactionId
+Type=type
+TransactionDate=transactionDate
+SettlementDate=settlementDate
+Units=units
+Price=price
+TotalConsideration=totalConsideration
+Currency=price/@currency
+ClientInternal=clientInternal
 enduse;
 
 -- Set variables for the portfolio's scope and code
@@ -33,7 +44,7 @@ SettlementDate as SettlementDate,
 Units as Units,
 Price as TradePrice,
 TotalConsideration as TotalConsideration,
-'GBP' as SettlementCurrency,
+Currency as SettlementCurrency,
 ClientInternal as ClientInternal
 from @txn_data;
 
