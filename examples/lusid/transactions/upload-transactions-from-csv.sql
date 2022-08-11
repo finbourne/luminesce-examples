18496596cc8a29e1e3a91170898cdddd49f7eaf8
fox8090 <56276220+fox8090@users.noreply.github.com>
2022-08-11 13:53:23 +0100

    SE-2341: Add example for csv file.

---
+++ examples/lusid/transactions/upload-transactions-from-csv.sql
@@ -0,0 +1,43 @@
+-- ============================================================
+
+-- Description:
+-- 1. In this query, we run an ETL process on some transactions.
+-- 2. First, we load a CSV file of transactions from Drive.
+-- 3. Next, we transform the shape of the transaction data.
+-- 4. Finally we upload the transaction data into LUSID.
+
+-- ============================================================
+
+-- Extract transaction data from LUSID Drive
+@txn_data = 
+use Drive.Csv
+--file=/luminesce-examples/transactions.csv
+enduse;
+
+-- Set variables for the portfolio's scope and code
+@@portfolio_scope =
+select 'IBOR';
+
+@@portfolio_code =
+select 'uk-equity';
+
+--Transform data using SQL
+@transactions = 
+select
+@@portfolio_scope as PortfolioScope,
+@@portfolio_code as PortfolioCode,
+TransactionID as TxnId,
+Type as Type,
+TransactionDate as TransactionDate,
+SettlementDate as SettlementDate,
+Units as Units,
+Price as TradePrice,
+TotalConsideration as TotalConsideration,
+Currency as SettlementCurrency,
+ClientInternal as ClientInternal
+from @txn_data;
+
+-- Upload the transformed data into LUSID
+select *
+from Lusid.Portfolio.Txn.Writer
+where ToWrite = @transactions;
