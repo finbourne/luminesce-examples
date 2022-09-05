7ad540bdde57a9c1691ae4505e4e6df7fdd273b0
ryangarvey <ryangarvey423@gmail.com>
2022-09-05 12:42:19 +0100

    Renumbering of files

--- examples/lusid/holdings/3-set-holdings.sql
+++ examples/lusid/holdings/3-set-holdings.sql
@@ -21,14 +21,29 @@ select 'UkEquity';
 select  @@portfolioScope as PortfolioScope,
         @@portfolioCode1 as PortfolioCode,
         @@today as EffectiveAt,
-        'BBG00WGHTKZ0' as Figi,
+        'LUID_00003D6Z' as LusidInstrumentId,
+        'GBP' as CostCurrency,
         100 as Units,
         12.3 as CostPrice,
-        'USD' as CostCurrency,
+        #2022-04-19# as PurchaseDate,
+        #2022-04-21# as SettleDate,
+        'QuantitativeSignal' as Strategy,
+        'Set' as WriteAction
+
+        union all
+
+select  @@portfolioScope as PortfolioScope,
+        @@portfolioCode1 as PortfolioCode,
+        @@today as EffectiveAt,
+        'LUID_00003D76' as LusidInstrumentId,
+        'GBP' as CostCurrency,
+        100 as Units,
+        12.3 as CostPrice,
         #2022-04-19# as PurchaseDate,
         #2022-04-21# as SettleDate,
         'QuantitativeSignal' as Strategy,
         'Set' as WriteAction;
+        
 
 -- Write table to the portfolio
 select * from Lusid.Portfolio.Holding.Writer where toWrite = @holding_data;
