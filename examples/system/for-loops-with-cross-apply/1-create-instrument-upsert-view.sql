d9440a6d551631fbe0d82bbbebccf4e2fa819759
Dan Barclay <dan.barclay@finbourne.com>
2023-03-08 15:43:07 +0000

    feature/FuzzySearch: Revert change

--- examples/system/for-loops-with-cross-apply/1-create-instrument-upsert-view.sql
+++ examples/system/for-loops-with-cross-apply/1-create-instrument-upsert-view.sql
@@ -4,28 +4,13 @@
 -- from a CSV file based on an EffectiveDate
 -- ============================================================
 
--- First delete the view if it already exists
-@delete_view =
-use Sys.Admin.SetupView
---provider=TestInstrumentWriter
---deleteProvider
-----
-select 1 as deleting
-enduse;
-
--- Wait 5 seconds after delete before attempting to re-create
--- This give the view time to reset on the Luminesce grid
-@delete_view_response =
-select *
-from @delete_view wait 5;
-
 @v =
 use Sys.Admin.SetupView
 --provider=TestInstrumentWriter
 --parameters
 FileName,Text,/luminesce-examples/instrument-test.csv,false
 EffectiveFrom,DateTime,2020-01-01,false
-----
+-----------------
 
 --Can't pass table parameters to the View, so for now have to read the file each time from Drive
 --Could have a separate query to chunk the files
