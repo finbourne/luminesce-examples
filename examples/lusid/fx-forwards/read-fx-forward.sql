73f9ab928e48baaa9231be8adf0d5be62c51c059
Stephen LeMasney <20342969+slemasne@users.noreply.github.com>
2023-03-08 21:52:51 +0000

    SE-2215: re org (#82)
    Co-authored-by: Stephen LeMasney <stephen.lemasney@finbourne.com>

--- examples/lusid/fx-forwards/read-fx-forward.sql
+++
@@ -1,9 +0,0 @@
--- ===============================================================
--- Description:
--- In this query, we read an Fx Forward holding from LUSID.
--- ===============================================================
-
--- Read Fx Forward from LUSID based on specified FIGI.
-
-select * from lusid.Instrument.FxForward
-where Figi = 'BBG000BVPXP1';
