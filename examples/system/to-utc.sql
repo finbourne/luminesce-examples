6c2446aa8aa05e16279cfd08949b5a5df8b2c31a
Stephen LeMasney <stephen.lemasney@finbourne.com>
2023-03-14 15:08:38 +0000

    HC-3225: Add to utc example

---
+++ examples/system/to-utc.sql
@@ -0,0 +1,37 @@
+--================================================================
+
+-- Description:
+-- In this query we demonstrate uses cases of the to_utc function
+-- which takes a string or datetime, with a timezone, and returns
+-- a UTC adjusted timezone string.
+
+--================================================================
+
+
+@data = use Drive.csv
+--file=/luminesce-examples/to-utc-datetimes.csv
+enduse;
+
+select 
+
+-- Convert ISO strings to UTC
+
+to_utc('2023-01-01', 'Europe/London') as stringDateAsUtc1,
+to_utc('2023-07-01 11:00:31', 'Europe/London') as stringDateSummerAsUtc1,
+to_utc('2023-01-01 10:00:00', 'US/Eastern') as stringDateAsUtc2,
+to_utc('2023-07-01', 'US/Eastern') as stringDateSummerAsUtc2,
+
+-- Convert datetimes to UTC
+
+to_utc(#2023-07-01#, 'Europe/Paris') as datetimeToUtc,
+
+
+-- Convert non ISO strings to UTC
+-- Intermediate conversion completed with the to_date function, which uses the following format
+-- https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
+
+to_utc(to_date(Date1 || ' ' || Time1, 'MM/dd/yyyy htt'), 'US/Pacific') as usFormatDateTime,
+to_utc(to_date(Date2 || ' ' || Time2, 'yyyy-MM-dd HH:mm'), 'Europe/Dublin') as utcFormatDateTime,
+to_utc(to_date(Date3 || ' ' || Time3, 'dd/MM/yyyy h:mmtt'), 'Europe/London') as ukFormatDateTime
+ 
+from @data;
