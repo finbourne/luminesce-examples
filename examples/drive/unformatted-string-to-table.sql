c5a6aedaa13b941e37fb3849b5b2f95cb4f994bb
Stephen LeMasney <20342969+slemasne@users.noreply.github.com>
2023-03-20 21:54:17 +0000

    Update unformatted-string-to-table.sql

--- examples/drive/unformatted-string-to-table.sql
+++ examples/drive/unformatted-string-to-table.sql
@@ -1,35 +1,39 @@
---============================================
-
 -- Description:
 -- In this example we load raw data from a 
 -- txt file into a Luminesce table. The 
 -- data is in a comma separated format
 
---============================================
-
-
-
 /*
-
-    1. Load comma separated string from txt file. 
-        The data will be loaded as one cell.
-
+====================================================
+    1. Load comma separated string from txt file
+====================================================
 */
 
 @@txtDataOriginal = use Drive.RawText
 --file=/luminesce-examples/prices-in-txt-file.txt
 enduse;
 
-@@txtData_RemoveRedundantTxt = select regexp_replace(@@txtDataOriginal, '|ADD |"', '');
+@@txtData_RemoveRedundantTxt = select regexp_replace(@@txtDataOriginal, '"|ADD| ', '');
 
 @@txtData_CommaSeparatedString = select regexp_match(@@txtData_RemoveRedundantTxt, '(?<=ENDRECORD)[.|\n|\W|\w]*');
 
 /*
-
-    2. Split string into separate rows
-
+======================================
+    2. Parse out the columns names 
+======================================
 */
 
+@@columnsWithNoSepCommas = select regexp_match(@@txtDataOriginal , '(?<=RECORD)([.|\n|\W|\w]*)(?=ENDRECORD)');
+
+@@columnsWithTrailingComma = select regexp_replace(@@columnsWithNoSepCommas, '\r\n', ',');
+
+@columnsWithSepCommas = select substr(@@columnsWithTrailingComma, 2, length(@@columnsWithTrailingComma) -2) as ColumnNames;
+
+/*
+==========================================
+    3. Split string into separate rows
+==========================================
+*/
 
 -- Note: This is how you split by endlines in Luminesce
 
@@ -41,33 +45,45 @@ and Original = @@txtData_CommaSeparatedString;
 where Value != '';
 
 /*
-
-    3. Use the Tools.Split provider to split the data into cells
-
+====================================================================    
+    4. Use the Tools.Split provider to split the data into cells    
+====================================================================
 */
+
+-- Split the table body first
 
 @tableRowsAsOneColWithIndex = select r.OriginalColumnValue, t.*
 from @tableRowsAsOneCol r
 inner join Tools.Split t 
 on t.Original = r.OriginalColumnValue;
 
+-- Then split column names
+
+@columnsNamesToTable = select r.ColumnNames, t.*
+from @columnsWithSepCommas r
+inner join Tools.Split t 
+on t.Original = r.ColumnNames;
+
+-- Join the table body and column names together
+
+@formattedTable = select 
+tr.Value as [RowValue], tr.[OriginalIndex], ct.Value as [ColumnName]
+from @columnsNamesToTable ct
+join @tableRowsAsOneColWithIndex tr on (ct.[Index] = tr.[Index])
+;
+
 /*
-
-    4. Pivot and format results
-
+=======================================
+    5. Pivot and format the results
+=======================================
 */
 
 @pivoted = 
-use Tools.Pivot with @tableRowsAsOneColWithIndex
---key=Index
---aggregateColumns=Value
+use Tools.Pivot with @formattedTable
+--key=ColumnName
+--aggregateColumns=RowValue
 enduse;
 
 select
-[1] as ClientInternal,
-[2] as PriceDate,
-[3] as PriceTime,
-[4] as Currency,
-[5] as Price,
-[6] as PriceField 
+*
 from @pivoted;
