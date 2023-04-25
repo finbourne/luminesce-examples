513e4849dc1bde8568a813105ecdb9a483168de7
Stephen LeMasney <20342969+slemasne@users.noreply.github.com>
2023-04-25 17:19:43 +0100

    update views (#100)
    Co-authored-by: Stephen LeMasney <stephen.lemasney@finbourne.com>

--- examples/file-orchestration/4-create-file-orchestration-view.sql
+++ examples/file-orchestration/4-create-file-orchestration-view.sql
@@ -6,56 +6,56 @@
 -- a "processed" directory
 
 
-@load_results = select 
+@load_results = 
+select 
 'Equity' as [Instrument],
-0 as [WriteErrorCode];
+0 as [WriteErrorCode],
+"instruments_success.csv" as [FileName]
+union 
+values
+('Equity', 103, "instruments_error.csv");
+
 
 @create_file_orchestration_view = use Sys.Admin.SetupView with @load_results
 --provider=ETL_Manager.File_Orchestration
 --description="This is a tool for moving files based off load results"
 --parameters
-FileName,Text,"instruments.csv",true
 NewDirectory,Text,"luminesce-examples/orchestration/new",true
-ErrorDirectory,Text,"luminesce-examples/orchestration/processed",true
-ProcessedDirectory,Text,"luminesce-examples/orchestration/error",true
+ErrorDirectory,Text,"luminesce-examples/orchestration/error",true
+ProcessedDirectory,Text,"luminesce-examples/orchestration/processed",true
 LoadResults,table,@load_results,true
 ----
 
 @@newDirectory = select #PARAMETERVALUE(NewDirectory);
 @@processedDirectory = select #PARAMETERVALUE(ProcessedDirectory);
 @@errorDirectory = select #PARAMETERVALUE(ErrorDirectory);
-@@fileName = select #PARAMETERVALUE(FileName);
+@loadResults = select * from #PARAMETERVALUE(LoadResults);
 
-@errorsFromLoad = select * from 
-#PARAMETERVALUE(LoadResults) where WriteErrorCode<>0;
+@filesWithErrors = 
+select distinct FileName, 
+@@errorDirectory as TargetDirectory
+from @loadResults where WriteErrorCode<>0;
 
-@writeErrors =select count(*) as [ResultCount]
-from @errorsFromLoad;
+@fileWithNoErrors = select 
+distinct FileName,
+@@processedDirectory as TargetDirectory
+from @loadResults where FileName not in (select FileName from @filesWithErrors);
 
-@targetFileResult = select iif(ResultCount = 0,
-(@@processedDirectory),
-(@@errorDirectory))
-as 'FileResult'
-from @writeErrors;
+@filesWithTarget = 
+select * from @filesWithErrors 
+union all
+select * from @fileWithNoErrors;
 
--- Print source file path
-@@newFileLocation = select @@newDirectory || '/' || @@fileName;
-@@printnewFileLocation =  select print('The source file path is:' || @@newFileLocation);
-
--- Print target directory
-@@targetDir = select FileResult from @targetFileResult;
-@@targetFullPath = select @@targetDir || '/' || @@fileName;
-@@printtargetFullPath = select print('The target directory is:' || @@targetFullPath);
-
-@updates =
-select @@newFileLocation as FullPath,
-@@targetFullPath  as [NewFullPath], 'MoveRenameMayOverwrite' as Operation;
+@filePathsToMove = 
+select 
+@@newDirectory || '/' || FileName as FullPath,
+TargetDirectory || '/' || FileName as NewFullPath,
+'MoveRenameMayOverwrite' as Operation
+from @filesWithTarget;
 
 @upload_files_to_drive = select *
 from Drive.file.Operation
-where UpdatesToPerform = @updates;
-
-select * from @upload_files_to_drive;
+where UpdatesToPerform = @filePathsToMove;
 
 enduse;
 
