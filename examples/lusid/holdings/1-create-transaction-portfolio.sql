499e3473a1fc14b2cbdadeab77f9977eadaa3898
Stephen LeMasney <stephen.lemasney@finbourne.com>
2023-08-17 15:26:21 +0100

    update holdings example docs

--- examples/lusid/holdings/1-create-transaction-portfolio.sql
+++ examples/lusid/holdings/1-create-transaction-portfolio.sql
@@ -1,27 +1,38 @@
--- ===============================================================
--- Description:
--- In this query, we create a Transaction Portfolio in LUSID
--- ===============================================================
+/*
 
--- Defining scope and code variables
-@@portfolioScope =
+----------------------------
+Create Transaction Portfolio
+----------------------------
 
-select 'luminesce-examples';
+Description:
 
-@@portfolioCode1 =
+    - In this query, we create a Transaction Portfolio in LUSID
 
-select 'UkEquity';
+More details:
 
--- Defining base currency and creation date
-@@base_currency =
+    - https://support.lusid.com/knowledgebase/category/?id=CAT-01099
 
-select 'GBP';
+*/
 
+@@portfolioScope = select 'luminesce-examples';
+@@portfolioCode1 = select 'UkEquity';
+
+/*
+
+Step 1: Defining base currency and creation date
+
+*/
+
+@@base_currency = select 'GBP';
 @@created_date = select #2000-01-01#;
 
--- Define the portfolio data
-@create_portfolio =
+/*
 
+Step 2: Define the portfolio data
+
+*/
+
+@create_portfolio =
 select   'Transaction' as PortfolioType, 
          @@portfolioScope as PortfolioScope, 
          @@portfolioCode1 as PortfolioCode, 
@@ -31,9 +42,13 @@ select   'Transaction' as PortfolioType,
          '' as SubHoldingKeys, 
          @@base_currency as BaseCurrency;
 
--- Upload the portfolio into LUSID
-@response_create_portfolio =
+/*
 
+Step 3: Upload the portfolio into LUSID
+
+*/
+
+@response_create_portfolio =
 select *
 from Lusid.Portfolio.Writer
 where ToWrite = @create_portfolio;
--- examples/lusid/holdings/4-adjust-holdings.sql
+++ examples/lusid/holdings/4-adjust-holdings.sql
@@ -1,38 +1,45 @@
--- =======================================================
--- Description:
--- In this query, we adjust the values of two holdings.
--- =======================================================
+/*
+
+---------------
+Adjust holdings
+---------------
+
+Description:
+
+    - In this query, we adjust the values of two holdings.
+
+More details:
+
+    - https://support.lusid.com/knowledgebase/category/?id=CAT-01099
+
+*/
 
 -- Defining scope and code variables
-@@portfolioScope =
+@@portfolioScope = select 'luminesce-examples';
 
-select 'luminesce-examples';
-
-@@portfolioCode1 =
-
-select 'UkEquity';
+@@portfolioCode1 = select 'UkEquity';
 
 -- Create new table to store two holdings' data
 @holding_data = 
-
-select  @@portfolioScope as PortfolioScope,
-        @@portfolioCode1 as PortfolioCode,
-        #2022-04-21# as EffectiveAt,
-        'GB0031348658' as ClientInternal,
-        'GBP' as CostCurrency,
-        100000 as Units,
-        'Adjust' as WriteAction
-
-        union all 
-
-select  @@portfolioScope as PortfolioScope,
-        @@portfolioCode1 as PortfolioCode,
-        #2022-04-21# as EffectiveAt,
-        'GB00BH0P3Z91' as ClientInternal,
-        'GBP' as CostCurrency,
-        500 as Units,
-        'Adjust' as WriteAction;
-
+select 
+@@portfolioScope as PortfolioScope,
+@@portfolioCode1 as PortfolioCode,
+#2022-04-21# as EffectiveAt,
+'GB0031348658' as ClientInternal,
+'GBP' as CostCurrency,
+100000 as Units,
+'Adjust' as WriteAction
+union all 
+select 
+@@portfolioScope as PortfolioScope,
+@@portfolioCode1 as PortfolioCode,
+#2022-04-21# as EffectiveAt,
+'GB00BH0P3Z91' as ClientInternal,
+'GBP' as CostCurrency,
+500 as Units,
+'Adjust' as WriteAction;
 
 -- Write table to the portfolio
-select * from Lusid.Portfolio.Holding.Writer where toWrite = @holding_data;
+select * from 
+Lusid.Portfolio.Holding.Writer
+where toWrite = @holding_data;
