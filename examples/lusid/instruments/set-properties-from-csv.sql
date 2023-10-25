56bad000e293470a035f74388239b8cc5fdb271c
Omar-Oz <81164562+Omar-Oz@users.noreply.github.com>
2023-10-25 17:55:27 +0100

    add.csv.properties.example (#135)
    * add.csv.properties.example
    
    * changes

---
+++ examples/lusid/instruments/set-properties-from-csv.sql
@@ -0,0 +1,29 @@
+-- Define file name, this is the instruments file on Drive
+
+@@file_name = select 'example_instruments.csv';
+
+-- Load data from Drive
+
+@instruments_data =
+use Drive.Csv with @@file_name
+--file=/instrument_test/{@@file_name}
+--addFileName
+enduse;
+
+@instProperties =
+select 
+ClientInternal as EntityId, 
+'ClientInternal' as EntityIdType, 
+'Instrument' as Domain,
+"Ratings" as PropertyScope, 
+RatingType as PropertyCode,
+Rating as Value,
+"Ratings" as EntityScope
+from @instruments_data;
+
+-- Upload the transformed data into LUSID
+
+select *
+from Lusid.Property.Writer
+where ToWrite = @instProperties
+limit 5;
