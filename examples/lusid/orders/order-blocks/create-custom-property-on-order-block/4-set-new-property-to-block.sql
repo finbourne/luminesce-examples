4f650b9b6fccffa32ba8ef6488489ee942a27bc9
keelan.robinson <keelan.robinson@finbourne.com>
2024-03-14 12:21:32 +0000

    removed spreadsheet read from 4

--- examples/lusid/orders/order-blocks/create-custom-property-on-order-block/4-set-new-property-to-block.sql
+++ examples/lusid/orders/order-blocks/create-custom-property-on-order-block/4-set-new-property-to-block.sql
@@ -1,24 +1,17 @@
 -- define contingent id
 
 @@contingent_id = SELECT '670';
+@@blockScope = SELECT 'blockUpdateExample';
+@@blockCode = SELECT 'ORD-BLKTEST-BLK';
 
--- Loading in block data from Excel spreadsheet
-@block_data_from_spreadsheet = 
-use Drive.Excel
---file=/luminesce-examples/order-blocks/block_seed.xlsx
---worksheet=blocks
---addFileName
-enduse;
 
 @blocks = SELECT
-bdfs.Block_Code as BlockCode, 
-bdfs.Block_Scope as BlockScope,
 @@contingent_id as Contingent_Id,
-b.*
-FROM @block_data_from_spreadsheet bdfs
-INNER JOIN Lusid.Block b
-ON bdfs.Block_Code = b.BlockCode
-AND bdfs.Block_Scope = b.BlockScope;
+*
+FROM Lusid.Block
+WHERE
+@@blockScope = BlockScope AND 
+@@blockCode = BlockCode;
         
 -- Write table to the block
 select * from Lusid.Block.Writer where toWrite = @blocks;
