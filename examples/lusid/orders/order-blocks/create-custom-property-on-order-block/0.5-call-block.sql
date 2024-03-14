6236f801e5e02fe079c64117b7d35d77539b6b10
keelan.robinson <keelan.robinson@finbourne.com>
2024-03-14 12:07:50 +0000

    Removed the spreadsheet read from the call block files

--- examples/lusid/orders/order-blocks/create-custom-property-on-order-block/0.5-call-block.sql
+++ examples/lusid/orders/order-blocks/create-custom-property-on-order-block/0.5-call-block.sql
@@ -6,26 +6,13 @@ call Block
 
 Description:
 
-    - In this query, we will Make sure the teardown worked, with columns displaying the properties available in our scope in Lusid.Block
+    - In this query, we will call the Blocks that i just set
 
 */
 
+@@blockScope = SELECT 'blockUpdateExample';
 
--- Loading in block data from Excel spreadsheet
-@block_data_from_spreadsheet = 
-use Drive.Excel
---file=/luminesce-examples/order-blocks/block_seed.xlsx
---worksheet=blocks
---addFileName
-enduse;
-
--- Defining scope and code variables
-
-@@blockScope =
-select bdfs.Block_Scope FROM @block_data_from_spreadsheet bdfs;
-
-@@blockCode =
-select bdfs.Block_Code from @block_data_from_spreadsheet bdfs ;
+@@blockCode = SELECT 'ORD-BLKTEST-BLK';
 
 select * from Lusid.Block
 where BlockCode= @@blockCode
