91971092801c7f3d221dfe46002cb94870437d94
keelan.robinson <keelan.robinson@finbourne.com>
2024-03-15 16:58:02 +0000

    Create  Custom button addition

---
+++ examples/lusid/orders/blocks/create-custom-button.sql
@@ -0,0 +1,61 @@
+@@providerName = select 'Set_Contingent_Order_ID_KR_five';
+@data = values
+  ('blockUpdateExample', "ORD-BLKTEST-BLK1");
+@block_ids = select
+  column1 as scope,
+  column2 as code
+FROM @data;
+
+--- Create the view
+@view = use Sys.Admin.SetupView with @@providerName, @block_ids
+--provider={@@providerName}
+--description="An example process that can be triggered for a set of Blocks"
+--parameters
+BlockIds,Table,@block_ids,true,Block scopes+codes
+----
+
+@block_ids = select * from #PARAMETERVALUE(BlockIds);
+
+-- todo use LUSID sequence id https://www.lusid.com/docs/api/#tag/Sequences
+
+
+-- Define contingent id
+@contingent_id = SELECT '888' as Contingent_Id;
+
+-- Add contingent Ids to the target blocks
+@block_ids_with_contingent_ids = SELECT 
+    b.scope,
+    b.code,
+    cid.Contingent_Id
+FROM 
+    @block_ids b
+INNER JOIN (@contingent_id) cid;
+
+-- Generate Blocks with updated values
+@blocks = SELECT
+biwci.Contingent_Id as Contingent_Id,
+b.* 
+FROM @block_ids_with_contingent_ids biwci
+INNER JOIN Lusid.Block b
+ON biwci.scope = b.BlockScope AND
+biwci.code = b.BlockCode;
+        
+-- Write updated values to the block
+@inserpt = select * from Lusid.Block.Writer where toWrite = @blocks;
+
+
+select "Contingent IDS written!" as result;
+
+enduse;
+@created = select * from @view;
+
+--- Set the requirement metadata on the view
+@metadata = values
+  (@@providerName, 'OrderBlotterBlockAction', 'True');
+@toWrite = select
+  column1 as ProviderName,
+  column2 as MetadataKey,
+  column3 as MetadataValue
+from @metadata;
+select * from Sys.Registration.Metadata.Writer
+  where ToWrite = @toWrite
