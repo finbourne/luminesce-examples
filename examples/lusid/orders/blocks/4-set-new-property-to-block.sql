137d0151f512c75a21fddf2aad03d5e4963783ff
keelan.robinson <keelan.robinson@finbourne.com>
2024-03-18 16:05:23 +0000

    Merged 2 queries into 1

--- examples/lusid/orders/blocks/4-set-new-property-to-block.sql
+++ examples/lusid/orders/blocks/4-set-new-property-to-block.sql
@@ -26,23 +26,15 @@ FROM @data;
 -- Define contingent id
 @contingent_id = SELECT '888' as Contingent_Id;
 
--- Add contingent Ids to the target blocks
-@block_ids_with_contingent_ids = SELECT 
-    b.scope,
-    b.code,
-    cid.Contingent_Id
-FROM 
-    @block_ids b
-INNER JOIN (@contingent_id) cid;
-
 -- Generate Blocks with updated values
 @blocks = SELECT
-biwci.Contingent_Id as Contingent_Id,
+cid.Contingent_Id,
 b.* 
-FROM @block_ids_with_contingent_ids biwci
+FROM @block_ids bi
 INNER JOIN Lusid.Block b
-ON biwci.scope = b.BlockScope AND
-biwci.code = b.BlockCode;
+ON bi.scope = b.BlockScope AND
+bi.code = b.BlockCode
+INNER JOIN (@contingent_id) cid;
         
 -- Write updated values to the block
 select * from Lusid.Block.Writer where toWrite = @blocks;
