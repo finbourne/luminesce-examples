eac9e5abe8ee0d64d7f52eb715fb035673730b9e
opower14 <160499982+opower14@users.noreply.github.com>
2025-03-28 10:15:13 +0000

    Update Upload_ECB_FX_Quotes.sql
    Updated descriptions and changed so all variables are used

--- examples/lusid/quotes/Upload_ECB_FX_Quotes.sql
+++ examples/lusid/quotes/Upload_ECB_FX_Quotes.sql
@@ -1,10 +1,13 @@
 -- =====================================================
--- Description:
--- In this query, we pull FX Spot data from the ECB Provider and upsert into Lusid
--- This can be run in query editor or used in a schedule
--- This can be used to upload all data from 1999-01-01 by changing start_date
--- Alternatively it can be used in a schdule to update FX values daily
--- Note FX rates are uploaded to ECB between 4-5pm GMT
+-- There are two use cases:
+--   1. Bulk upload of all data from 1999
+--       By changing @@start_date from current date to '1999-01-01' 
+--       this uploads all FX Rates from ECB
+--
+--   2. End of Day scheduled upsert of todays data 
+--       Running the query with @@start_date being select date('now') at 6pm CET
+--       uploads that days FX Rates from the ECB to Lusid
+--       see the ECB website for details on when todays rates are available
 -- =====================================================
 @@start_date = select date('now'); --Gets todays date, alternatively provide your start date
 @@scope = select 'ECB_FX_test'; --Sets scope
@@ -14,20 +17,20 @@
 
 --Format Quote Request
 @table_of_data = 
-select 'ECB_FX_test' as QuoteScope, 
-'Lusid' as Provider, 
-'ECB' as PriceSource, 
+select @@scope as QuoteScope, 
+@@provider as Provider, 
+@@price_source as PriceSource, 
 CurrencyDenominator || '/' || Currency as InstrumentId, 
 'CurrencyPair' as InstrumentIdType, 
 'Rate' as QuoteType, 
-'mid' as Field, 
+@@field as Field, 
 ObservationValue as Value, 
 Currency as Unit, 
 TimePeriod as QuoteEffectiveAt 
 from [Ecb.Exchangerates] --Pulls Data from ECB Provider and places into Quote Format
-where StartPeriod = date('now', '-1 days') --Upload data from this date
-and ExchangeRateType = 'SP00' --Pulls only FX Spot Rates
-and Frequency = 'D'; --Filters out weekly and monthly data
+where StartPeriod = @@start_date --Upload data from this date
+and ExchangeRateType is 'SP00' --Pulls only FX Spot Rates
+and Frequency is 'D'; --Filters out weekly and monthly data
 
 --Sends Quote Request
 select * from Lusid.Instrument.Quote.Writer 
