1ffc778b045d440d019514a58fc109f9267e6e5c
opower14 <160499982+opower14@users.noreply.github.com>
2025-03-28 12:00:24 +0000

    Update Upload_ECB_FX_Quotes.sql
    Update description and variable usage

--- examples/lusid/quotes/Upload_ECB_FX_Quotes.sql
+++ examples/lusid/quotes/Upload_ECB_FX_Quotes.sql
@@ -1,4 +1,8 @@
 -- =====================================================
+-- Description:
+-- This script is an example of how to use the ECB Provider 
+-- in Lusid to upload FX Rates into Lusid
+--
 -- There are two use cases:
 --   1. Bulk upload of all data from 1999
 --       By changing @@start_date from current date to '1999-01-01' 
@@ -9,26 +13,21 @@
 --       uploads that days FX Rates from the ECB to Lusid
 --       see the ECB website for details on when todays rates are available
 -- =====================================================
-@@start_date = select date('now'); --Gets todays date, alternatively provide your start date
-@@scope = select 'ECB_FX_test'; --Sets scope
-@@price_source = select 'ECB';
-@@field = select 'mid';
-@@provider = select 'Lusid';
 
 --Format Quote Request
 @table_of_data = 
-select @@scope as QuoteScope, 
-@@provider as Provider, 
-@@price_source as PriceSource, 
-CurrencyDenominator || '/' || Currency as InstrumentId, 
-'CurrencyPair' as InstrumentIdType, 
-'Rate' as QuoteType, 
-@@field as Field, 
-ObservationValue as Value, 
-Currency as Unit, 
-TimePeriod as QuoteEffectiveAt 
+select 'scope' as QuoteScope, --Change variables below to your desired values
+'provider' as Provider,
+'price source' as PriceSource,
+CurrencyDenominator || '/' || Currency as InstrumentId, -- All Below Variables are standard and cannot be changed
+'CurrencyPair' as InstrumentIdType,
+'Rate' as QuoteType,
+'mid' as Field,
+ObservationValue as Value,
+Currency as Unit,
+TimePeriod as QuoteEffectiveAt
 from [Ecb.Exchangerates] --Pulls Data from ECB Provider and places into Quote Format
-where StartPeriod = @@start_date --Upload data from this date
+where StartPeriod = select date('now'), --Upload data from this date. select date('now') for todays only or '1999-01-01' to upload all data
 and ExchangeRateType is 'SP00' --Pulls only FX Spot Rates
 and Frequency is 'D'; --Filters out weekly and monthly data
 
